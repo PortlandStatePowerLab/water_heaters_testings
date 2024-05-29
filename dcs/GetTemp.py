abc0adbdca0ae420a798743bacbea6173b05d27a
Paresa <122493760+dparesa@users.noreply.github.com>
2024-05-29 09:33:16 -0700

    Update GetTemp.py

--- dcs/GetTemp.py
+++ dcs/GetTemp.py
@@ -6,25 +6,35 @@ import csv
 
 ########################  DEFINITIONS  ####################################
 
-def read_temp(device, decimals=1, sleeptime=60):
+def read_temp(device, decimals=1):
     """Reads the temperature from a 1-wire device"""
     while True:
         try:
             timepoint = datetime.datetime.now()
             with open(device, "r") as f:
                 lines = f.readlines()
+
+            # Ensure lines is not empty and has at least two lines
+            if not lines or len(lines) < 2:
+                #time.sleep(0.2)
+                continue
+
             while lines[0].strip()[-3:] != "YES":
-                time.sleep(0.2)
+                #time.sleep(0.2)
                 with open(device, "r") as f:  # Change to read_temp_raw()
                     lines = f.readlines()
+
+                # Ensure lines is not empty and has at least two lines
+                if not lines or len(lines) < 2:
+                    #time.sleep(0.2)
+                    continue
+
             timepassed = (datetime.datetime.now() - timepoint).total_seconds()
             equals_pos = lines[1].find("t=")
             if equals_pos != -1:
-                temp_string = lines[1][equals_pos + 2:]
+                temp_string = lines[1].strip()[equals_pos + 2:]
                 temp = round(float(temp_string) / 1000.0, decimals)
                 temp = round(9 / 5 * temp + 32, 3)
-                #print(time.strftime("%H:%M:%S - ") + str(temp) + " F")
-                time.sleep(sleeptime - timepassed)
                 timepoint = datetime.datetime.now()
                 return timepoint, temp  # Return timestamp and temperature
         except KeyboardInterrupt:
@@ -45,16 +55,15 @@ if not os.path.exists('templog'):
 ##############################  MAIN  #############################################
 
 
-
 if __name__ == "__main__":
-    WH_1 = "28-00043d6393ff"
-    WH_2 = "28-000008e55d0d"
+    WH_1 = "28-00043ca024ff"
+    WH_2 = "28-00043d6393ff"
     WH_3 = "28-00043d6037ff"
     WH_4 = "28-00043ca043ff"
 
     device_id_1 = "28-000008e55d0d"  # WH_ALL ambient temp device ID
     device_id_2 = "28-0416c138deff"  # Cold Water temp device ID
-    device_id_3 = WH_4  # WH Hot temp device ID **UNIQUE TO EACH WH**
+    device_id_3 = WH_1  # WH Hot temp device ID **UNIQUE TO EACH WH**
 
     device_path_1 = f"/sys/bus/w1/devices/{device_id_1}/w1_slave"
     device_path_2 = f"/sys/bus/w1/devices/{device_id_2}/w1_slave"
@@ -114,13 +123,17 @@ if __name__ == "__main__":
     for com in DRcom:
         data_name = wh_type + volume + '_TEMPDATA_' + com[0] + '.csv'
 
-        start_time = time.time()
-        while time.time() - start_time < 60:  # Stay in the loop for 60 seconds
+        i = 0
+
+        while i  < 60*24*2:  # Stay in the loop for 60 seconds
             timestamp_1, temperature_1 = read_temp(device_path_1)
             timestamp_2, temperature_2 = read_temp(device_path_2)
             timestamp_3, temperature_3 = read_temp(device_path_3)
+
             temp_data = [timestamp_1.strftime("%Y-%m-%d %H:%M:%S"), temperature_1, temperature_2, temperature_3]
 
             print(f'{timestamp_1.strftime("%H:%M:%S")} | AMBIENT: {temperature_1} F | COLD: {temperature_2} F | HOT: {temperature_3} F')
 
+            i += 1
+            time.sleep(57)
             write_to_csv(data_name, temp_data)
