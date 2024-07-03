69dff664ff4fa7a04bff68d68699d6993bcd9b46
Paresa <122493760+dparesa@users.noreply.github.com>
2024-07-03 14:23:08 -0700

    Create CheckCurrent.py
    dana use this to check the CT

---
+++ dcs/sample2/sample2/CheckCurrent.py
@@ -0,0 +1,21 @@
+#include <wiringPi.h>
+#include <mcp3004.h>
+#include "mcp3008.h"
+#include <iostream>
+
+mcp3008::mcp3008(int channel) : channel_(channel){
+wiringPiSetupGpio();
+mcp3004Setup(channel, 0);
+};
+
+mcp3008::~mcp3008(){};
+
+using namespace std;
+
+float mcp3008::GetCurrent(){
+        float voltage = float(analogRead(channel_)) / 1023 * 4.4; // 4.4 is Vref
+        float current = voltage * 10; // factor dependent on CT setting
+        cout << "\nVoltage check: " << voltage << endl; # prints out voltage value from CT
+        cout << "\nCurrent check: " << current << endl; # prints out current from CT
+        return current;
+};
