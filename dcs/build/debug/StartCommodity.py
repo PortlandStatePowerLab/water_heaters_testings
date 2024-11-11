140dcc0fbfab8828f50641160ddefe0881271413
Paresa <122493760+dparesa@users.noreply.github.com>
2024-11-11 10:11:34 -0800

    Update StartCommodity.py
    add comments

--- dcs/build/debug/StartCommodity.py
+++ dcs/build/debug/StartCommodity.py
@@ -5,15 +5,17 @@ import signal
 import csv
 from datetime import datetime, timedelta
 
+
+# assign data file name
 wh_type = input('WH Brand:')
 volume = input('Capacity (gallons):')
 
 yes = ['y', 'Y', 'yes', 'Yes', 'YES']
 
-
 arguments = []
 DRcom = []
 
+# add mode to queue for 48-hr conformance test
 baseline = input('Do you want to test Baseline (y/n)?')
 if baseline in yes:
     arguments.append(['e\n'])
@@ -39,14 +41,15 @@ if lu in yes:
     arguments.append(['l\n'])
     DRcom.append(['LoadUp'])
 
-
+# let user know which tests will be performed
 print('Testing for the following:')
 for com in DRcom:
     print(com[0])
 
-
+# create outside comm input
 outsideComm = 'o\n'
 
+# input file location - copy to output file
 input_file = 'log.csv'
 
 process = None
@@ -54,6 +57,11 @@ process = None
 ########   DEFINITIONS    ######################################
 
 def start_commodity(mode):
+    '''
+    This function initializes the sample2 program
+    provided by EPRI. 
+
+    '''
     # open commodity service
     global process
     process = subprocess.Popen(['./sample2'], stdin=subprocess.PIPE)
@@ -66,6 +74,13 @@ def start_commodity(mode):
     time.sleep(5)
 
 def update_csv(input_file, output_file, last_line):
+'''
+This function will copy each line from
+the log.csv file to the new output file
+that is named according to brand, volume, mode.
+
+'''
+
     with open(input_file, 'r') as input_csv, open(output_file, 'a') as output_csv:
         reader = csv.reader((row.replace('\0','') for row in input_csv), delimiter=',')
         writer = csv.writer(output_csv)
@@ -77,6 +92,10 @@ def update_csv(input_file, output_file, last_line):
 
 # end commodity service
 def end_service():
+'''
+This function kills
+commodity service. 
+''' 
     os.kill(process.pid, signal.SIGINT)
     process.wait()
     time.sleep(5)
@@ -92,6 +111,7 @@ startTime = input('Would you like to start Commodity Service immediately (y/n)?'
 
 no = (['n', 'N', 'no', 'NO', 'No', 'nO'])
 
+# calculate delay time to start commodity service
 if startTime in no:
     if now > run_at:
         run_at += timedelta(days=1)
@@ -108,22 +128,26 @@ else:
 # Wait until the desired time
 time.sleep(delay)
 
-
+# start 48 conformance test
 for arg,com in zip(arguments, DRcom):
-
+    # create unique output file name according to mode
     output_file = 'testlog/'+wh_type+volume+'_'+com[0]+'.csv' # create new csv output file name
 
+    # count number of lines to slice for new output file
     with open(input_file, 'r', newline='', encoding='utf-8') as input_csv:   # slice log.csv file
         reader = csv.reader((row.replace('\0','') for row in input_csv), delimiter=',')
         last_line = sum(1 for row in reader) - 1
-
+        
+    # start commodity service
     start_commodity(outsideComm)
 
     process.stdin.write((arg[0].encode()))
     process.stdin.flush()
 
+    # initialize test period duration
     sleep_timer = 60*60*24*2  # sleep for 2 days
 
+    # run until test period is DONE
     while sleep_timer > 0:
         # send outside comm every 10 minutes
         time.sleep(60 * 5) # sleep 5 minutes
@@ -138,7 +162,7 @@ for arg,com in zip(arguments, DRcom):
 
         sleep_timer -= 60 * 10 # reduce time by 10 minutes for 2 days
 
-
+    # create and save output file
     update_csv(input_file, output_file, last_line) # create csv output file
 
     time.sleep(5)
